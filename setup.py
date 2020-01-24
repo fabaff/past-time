93728975b36f3cef5ab5ac046f6c38a36ba31f82
Fabian Affolter <mail@fabian-affolter.ch>
2020-01-24 21:24:15 +0100

    Works with Python 3.9

--- setup.py
+++ setup.py
@@ -42,6 +42,7 @@ setup(
         "Operating System :: POSIX",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
         "Topic :: Utilities",
     ],
     entry_points={"console_scripts": ["past-time = past_time.__init__:cli"]},
